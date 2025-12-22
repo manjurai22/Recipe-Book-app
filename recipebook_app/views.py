22f0cb5cc16f06a5b4485612cb0f932206a09615
manjurai22 <manjurai611@gmail.com>
2025-12-22 17:36:03 +0545

    recipe-list

--- recipebook_app/views.py
+++ recipebook_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from recipebook_app import Recipe
 # Create your views here.
+
+def recipe_list(request): 
+    recipes = Recipe.objects.all()
+    return render(
+        request,
+        "recipe_list.html",
+        {"recipes": recipes}
+    )
