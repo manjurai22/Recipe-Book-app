466736a8df033aaa9a9ca942b93bf4246ff1e11e
manjurai22 <manjurai611@gmail.com>
2025-12-23 17:06:50 +0545

    add-recipe

--- recipebook_app/views.py
+++ recipebook_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from recipebook_app.models import Recipe
 
 # Create your views here.
@@ -10,3 +10,16 @@ def recipe_list(request):
         "recipe_list.html",
         {"recipes": recipes}
     )
+
+def add_recipe(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("recipe_list")  # Redirect to the recipe list page after adding
+    else:
+        form = RecipeForm()  # Empty form for GET request
+
+    return render(request, "add_recipe.html", {"form": form})
+
+
